892f172c84068478cd261b9690b338163405058f
Xiyuan Sun <xiyuansu@iastate.edu>
2019-04-17 12:08:54 -0500

    updated the data retrieving

--- code/get_and_clean_data.R
+++ code/get_and_clean_data.R
@@ -1,6 +1,24 @@
 #retrieve data
 
-jsonlite::read_json("https://data.iowa.gov/resource/m3tr-qhgy.json")
+library(readr)
+Iowa_Liquor_Sales_Story <- read_csv("~/Desktop/stat585Lab4/data/Iowa_Liquor_Sales-Story.csv")
+View(Iowa_Liquor_Sales_Story)
+initialjson <- jsonlite::read_json("https://data.iowa.gov/resource/m3tr-qhgy.json")
+test_json_file <- initialjson[[1]]
+invoice <- test_json_file$invoice_line_no
+date <- gsub("[A-z].*$","",test_json_file$date)
 
 
+
+# install.packages("devtools")
+# devtools::install_github("Chicago/RSocrata")
+# 
+# library("RSocrata")
+# 
+# df <- read.socrata(
+#   "https://data.iowa.gov/resource/m3tr-qhgy.json",
+#   app_token = "YOURAPPTOKENHERE",
+#   email     = "user@example.com",
+#   password  = "fakepassword"
+# )
 #clean data
