9d3822072cff04cd4e0afca5fa921d0c93ad7d01
Xiyuan Sun <xiyuansu@iastate.edu>
2019-04-17 23:23:24 -0500

    tried aggregated dateData

--- shiny/server.R
+++ shiny/server.R
@@ -1,7 +1,7 @@
 library(shiny)
 #load the data
 cleaned_data <- as.data.frame(story_liquor_cleaned)
-head(cleaned_data)
+
 #make up a new response variable: saledollars/volumesoldliters
 shiny_raw_data <- cleaned_data %>%
   mutate(dollar_per_liter =saledollars/volumesoldliters)%>%
@@ -9,32 +9,51 @@ shiny_raw_data <- cleaned_data %>%
 
 #time series plot
 #overall dollar_per_liter means on each date
-dollar_per_liter_means <- aggregate(shiny_raw_data$dollar_per_liter, by=list(shiny_raw_data$date),
-                                    FUN=mean, na.rm=T)
-colnames(dollar_per_liter_means) = c("date", "dollar_per_liter_mean")
+# dollar_per_liter_means <- aggregate(shiny_raw_data$dollar_per_liter, by=list(shiny_raw_data$date),
+#                                     FUN=mean, na.rm=T)
+# colnames(dollar_per_liter_means) = c("date", "dollar_per_liter_mean")
 
 #aggregate dollar_per_liter by catsimp on each date
-
+# factor could be any level of catsimp (13 levels in total)
+levels(as.factor(shiny_raw_data$catsimp))
 #generate a list of length 1272
 #each list element represent one day
 dateData <- lapply(unique(shiny_raw_data$date), 
                    function(x){shiny_raw_data[shiny_raw_data$date==x, ]})
 
+#test the factorData function
+f="amaretto"
+v<- "dollar_per_liter"
+
+shiny_factor_data <- shiny_raw_data %>% filter(catsimp==f)
+factor_dateData <- lapply(unique(shiny_factor_data$date), 
+         function(x){shiny_factor_data[shiny_factor_data$date==x, ]})
+#loop for each date
+
+
+# for example: f="amaretto"
+factorData <- lapply(factor_dateData, function(x){
+  # subset x with carsimp==f
+  x <- x[which(x$catsimp==f), ]
+  meanDat <- as.data.frame(t(tapply(x[,v],x$date, function(z) mean(z, na.rm = T))))
+  names(meanDat) <- paste0(v, "_mean")
+  #combine data into a single data object
+  means <- cbind(unique(x$date), f, meanDat)
+  names(means)[1] <- "date"
+  return(means)
+})
+
+factorData <- Reduce(x=factorData, f=rbind)
+
+
+factorResult <- factorData[order(as.Date(factorData$date)),]
+
+
 #time series plot using ggplot2
 library(ggplot2)
 theme_set(theme_minimal())
-ggplot(data = liquor_data, aes(x = date, y = sale_dollars))+geom_point()
-library(dplyr)
-test_large_data <- as.data.frame(Iowa_Liquor_Sales_Story)
-
-test_single_date_data <- test_large_data %>% 
-  filter(Date=="03/29/2019") %>%
-  mutate(var1 = `Sale (Dollars)`/`Volume Sold (Liters)`, var2 = `State Bottle Retail`/`Bottle Volume (ml)`*1000)
-
-levels(as.factor(test_single_date_data$City)) #2 cities
-levels(as.factor(test_single_date_data$`Store Name`)) #9 stores
-levels(as.factor(test_single_date_data$`Category Name`)) #28 categories
-levels(as.factor(test_single_date_data$`Vendor Name`)) #24 vendors
+ggplot(data = factorResult, aes(x = date, y = dollar_per_liter_mean))+ 
+  geom_line()
 
 
 
