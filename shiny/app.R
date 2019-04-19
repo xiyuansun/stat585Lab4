d49d5a435b0055cf642a6c3ee518300b867733e4
goodekat <kgoode@iastate.edu>
2019-04-18 21:23:15 -0500

    worked on spatial part of app

--- shiny/app.R
+++ shiny/app.R
@@ -1,8 +1,13 @@
 # Shiny app for visualizing Story County liquor sales
 
+# Load libraries
+library(plotly)
 library(maps)
 library(shiny)
 library(tidyverse)
+
+# Read in necessary data
+story_spatial_data <- readRDS("../data/story_spatial_data.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -13,18 +18,16 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput("category", 
+                     label = "Liquor Category", 
+                     choices = levels(story_spatial_data$catsimp))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
         tabsetPanel(type = "tabs",
                     tabPanel("Temporal", plotOutput("distPlot")),
-                    tabPanel("Spatial", plotOutput("storymap"))
+                    tabPanel("Spatial", plotlyOutput("storymap"))
         )
       )
    )
@@ -32,25 +35,30 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-   
-   # Obtain map data for Story County
-   story_county <- map_data("county") %>% filter(region == "iowa", subregion == "story")
-   
-   output$storymap <- renderPlot({
-     
-     ggplot(story_county , aes(x = long, y = lat, group = group)) + 
-       geom_polygon(alpha = 0.1, color = "black") + 
+  
+  # TEMPORAL VISUALIZATIONS ------------------------------------------------------------
+  
+  # SPATIAL VISUALIZATIONS -------------------------------------------------------------
+  
+  # Obtain map data for Story County
+  story_county <- map_data("county") %>% filter(region == "iowa", subregion == "story")
+  
+  # Create spatial plot
+  output$storymap <- renderPlotly({
+    
+    # Create plot
+    spatial_plot <- ggplot() + 
+      geom_polygon(data = story_county, 
+                    aes(x = long, y = lat, group = group), 
+                    alpha = 0.1, 
+                    color = "black") + 
+       geom_jitter(data = story_spatial_data %>% filter(catsimp == input$category), 
+                  aes(x = longitude, y = latitude, color = n, size = n)) + 
        labs(x = "Longitude", y = "Latitude", title = "Story County")
    
+    # Make plot interactive
+    ggplotly(spatial_plot, width = 800, height = 600)
+     
     })
    
 }
